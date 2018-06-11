ed105a195a33ca65db901ad28599cabda13d9814
Kevin He <hehongqiang@lakala.com>
2018-06-11 20:45:29 +0800

    a few modification for InvokeService.java

--- AnnotationDemo/src/main/java/com/hhq/test/annotation/controller/verifyservice/InvokeService.java
+++ AnnotationDemo/src/main/java/com/hhq/test/annotation/controller/verifyservice/InvokeService.java
@@ -1,13 +1,15 @@
- package com.hhq.test.annotation.controller.verifyservice;
+package com.hhq.test.annotation.controller.verifyservice;
 
- /**
+/**
  * @author Kevin He
  * @date 2018/05/24
  * @version 1.0.0
  */
 public interface InvokeService {
+
     /**
      * Invoke the remote service
+     * 
      * @param requestBody
      * @return
      */
